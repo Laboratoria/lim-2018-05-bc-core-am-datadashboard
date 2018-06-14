9093a99339c68482a216dfe0313577384236397f
Florball <flor_ballinas@hotmail.com>
2018-06-14 09:05:21 -0500

    bjando cambios dalai

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// const btnLogging = document.getElementById('logging');
+// const user = document.getElementById('user').value;
+// const password = document.getElementById('password').value;
+// btnLogging.addEventListener('click', () => {
+//   if (user == true && password == true) {
+
+//   }
+// });
