2701abc5b94542547d2e6a5b9341f53d9dd78476
Florball <flor_ballinas@hotmail.com>
2018-06-15 10:02:38 -0500

    funcion para añadir tabs terminada, select lim de prueba  eliminada y nuevas clases en css

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-// const btnLogging = document.getElementById('logging');
-// const user = document.getElementById('user').value;
-// const password = document.getElementById('password').value;
-// btnLogging.addEventListener('click', () => {
-//   if (user == true && password == true) {
-
-//   }
-// });
+// funcion para ocultar logging y mostrar formulario
+let btnLogging = document.getElementById('logging');
+let newTab = document.getElementById('newTab');
+let loggingIn = document.getElementById('loggingIn')
+btnLogging.addEventListener('click', function(){
+  loggingIn.classList.replace('show','hide');
+  newTab.classList.remove('hide');
+  newTab.classList.add('show');
+});
