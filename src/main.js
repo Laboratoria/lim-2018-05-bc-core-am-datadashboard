f44995d518d28e34fdb8e4d159ba8bf2233abf1d
DaliaES <dalia.escuderov@gmail.com>
2018-06-18 10:22:35 -0500

    correccion de error en funcion para mostrar cohorts

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 // funcion para ocultar logging y mostrar formulario
+
 let btnLogging = document.getElementById('logging');
 let newTab = document.getElementById('newTab');
-let loggingIn = document.getElementById('loggingIn')
+let loggingIn = document.getElementById('loggingIn');
+
 btnLogging.addEventListener('click', function(){
   loggingIn.classList.replace('show','hide');
   newTab.classList.remove('hide');
   newTab.classList.add('show');
+});
+
+//Mostrar lista de cohorts dando click en boton
+cohort.addEventListener('click', (event) => {
+  event.preventDefault();
+  getcohort();
 });
