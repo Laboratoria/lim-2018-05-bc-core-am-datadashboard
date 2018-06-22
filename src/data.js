25a86dcdeace5a18f7dd532815fb1e19242a802d
DaliaES <dalia.escuderov@gmail.com>
2018-06-22 08:22:47 -0500

    subiendo funcion cohorts, users y progress

--- src/data.js
+++ src/data.js
@@ -1,23 +1,43 @@
-const cohort = document.getElementById("laboratoria2");
-const list = document.getElementById("listaCohorts");
+const urlCohort = "../data/cohorts.json"
+const urlUser = "../data/cohorts/lim-2018-03-pre-core-pw/users.json"
+const urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json"
 
-const getcohort = () => {
-  let xhr = new XMLHttpRequest();
-  //console.log(xhr);	
-
-  xhr.open("GET", "../data/cohorts.json");
-  xhr.onload = getusers;
-  xhr.onerror = handlerror;
+//funcion para obtener datos json
+const getData = (url,callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onload = callback;
   xhr.send();
-}
-const getusers = (event) => {
-  let data = JSON.parse(event.target.responseText);
-  console.log(data);
-  data.forEach(curso => {
-    var lista = document.createElement('li');
-    lista.value = curso.id;
-    lista.innerHTML = curso.id;
-    list.appendChild(lista);
-  });
-}
-const handlerror = () => alert("Hay un error");
+};
+
+// funcion para llamar lista de cohorts
+const getCohorts = () => {
+  return JSON.parse(event.target.responseText);
+};
+
+//funcion para llamar lista de estudiantes  
+const getUsers = () => {
+ // const data2 = JSON.parse(event.target.responseText);
+  return JSON.parse(event.target.responseText);
+     
+};
+
+
+const getProgress = (id) =>{
+ 
+  let data3 = JSON.parse(event.target.responseText);
+  
+    if(data3){
+    
+     // console.log(usersWithStats);
+      
+      return data3[id].intro
+    };
+
+  
+  return false;
+
+  }
+// window.computerUsersState(users,progress,courses)
+// id de users=id Progress bote info del progres(for of)
+
