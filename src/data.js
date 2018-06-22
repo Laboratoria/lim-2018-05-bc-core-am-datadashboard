1a08d977b9486594390f37033b0eae7ab22ab230
DaliaES <dalia.escuderov@gmail.com>
2018-06-22 09:27:33 -0500

    subiendo cambios html y js funcion

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const urlUser = "../data/cohorts/lim-2018-03-pre-core-pw/users.json"
 const urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json"
 
 //funcion para obtener datos json
-const getData = (url,callback) => {
+const getData = (url, callback) => {
   const xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.onload = callback;
@@ -17,27 +17,27 @@ const getCohorts = () => {
 
 //funcion para llamar lista de estudiantes  
 const getUsers = () => {
- // const data2 = JSON.parse(event.target.responseText);
+  // const data2 = JSON.parse(event.target.responseText);
   return JSON.parse(event.target.responseText);
-     
+
 };
 
 
-const getProgress = (id) =>{
- 
-  let data3 = JSON.parse(event.target.responseText);
-  
-    if(data3){
-    
-     // console.log(usersWithStats);
-      
-      return data3[id].intro
-    };
+const getProgress = (id) => {
 
-  
+  let data3 = JSON.parse(event.target.responseText);
+
+  if (data3) {
+
+    // console.log(usersWithStats);
+
+    return data3[id].intro
+  };
+
+
   return false;
 
-  }
+}
 // window.computerUsersState(users,progress,courses)
 // id de users=id Progress bote info del progres(for of)
 
--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ let laboratoria1 = document.getElementById('laboratoria1');
 let laboratoriaBar = document.getElementById('laboratoriaBar');
 let btnLima = document.getElementById('lima');
 
-btnLogging.addEventListener('click', function(){
+btnLogging.addEventListener('click', function () {
   let laboratoria2 = document.getElementById('laboratoriaBar');
-let navigatingBar = document.getElementById('navigatingBar');
-let loggingIn = document.getElementById('loggingIn');
+  let navigatingBar = document.getElementById('navigatingBar');
+  let loggingIn = document.getElementById('loggingIn');
 
-  if (true || (document.form.password.value=='CONTRASEÑA' && document.form.user.value=='USUARIO')){ 
+  if (true || (document.form.password.value == 'CONTRASEÑA' && document.form.user.value == 'USUARIO')) {
     loggingIn.classList.replace('show', 'hide');
-    
+
     navigatingBar.classList.replace('hide', 'show');
 
     laboratoria2.classList.replace('hide', 'show');
-  } else{ 
-    alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
-    } 
+  } else {
+    alert("Porfavor ingrese, nombre de usuario y contraseña correctos.");
+  }
 });
 
 
@@ -25,8 +25,8 @@ let loggingIn = document.getElementById('loggingIn');
 const cohort = document.getElementById("newTab");
 cohort.addEventListener('click', (event) => {
   event.preventDefault();
- 
-  getData(urlCohort,  function(){
+
+  getData(urlCohort, function () {
     let laboratoria2 = document.getElementById('laboratoria2');
     laboratoria2.classList.replace('hide', 'show');
     navigatingBar.classList.replace('show', 'hide');
@@ -35,65 +35,64 @@ cohort.addEventListener('click', (event) => {
       const list = document.getElementById("listaCohorts");
       const a = document.createElement('a');
       const lista = document.createElement('li');
-      a.setAttribute('href', '#'); 
-      a.setAttribute('id', group.id); 
+      a.setAttribute('href', '#');
+      a.setAttribute('id', group.id);
       a.innerHTML = group.id;
       lista.appendChild(a);
       list.appendChild(lista);
       const liuser = document.getElementById('aqp-2018-03-jp-core-talento')
       addEventToCohortElem(liuser);
     };
-  }); 
+  });
 });
 
 window.addEventToCohortElem = (elem) => {
-  elem.addEventListener('click', (event)=>{
+  elem.addEventListener('click', (event) => {
     event.preventDefault();
-    getData(urlUser,()=>{
+    getData(urlUser, () => {
       for (const group of getUsers()) {
         const list = document.getElementById("usuarios");
+        const a = document.createElement('a');
+        const lista = document.createElement('li');
+        // a.setAttribute('href', '#'); 
+        a.setAttribute('id', group.id);
+        a.innerHTML = group.name;
+        lista.appendChild(a);
+        list.appendChild(lista);
+        addEventToUserElem(a);
+      }
+    })
+
+  })
+};
+
+window.addEventToUserElem = (elem) => {
+  elem.addEventListener('click', () => {
+    event.preventDefault();
+    getData(urlProgress, function () {
+      const id = elem.getAttribute('id');
+      const data = getProgress(id);
+      if (data) {
+        //for (const group of data) {
+
+        console.log(data)
+        /**       
+           const list = document.getElementById("usuarios");
         const a = document.createElement('a');
         const lista = document.createElement('li');
         a.setAttribute('href', '#'); 
         a.setAttribute('id', group.id); 
         a.innerHTML = group.name;
         lista.appendChild(a);
-        list.appendChild(lista);
-        addEventToUserElem(a); 
-      }
-    })
-     
-})
-};
+        list.appendChild(lista); 
+  */
 
-window.addEventToUserElem = (elem) => {
-
-  elem.addEventListener('click', ()=>{
-   
-    event.preventDefault();
-    getData(urlProgress,function(){
-      const id = elem.getAttribute('id');
-      const data  =  getProgress(id);
-      if(data){
-        //for (const group of data) {
-              console.log(data)
-          /**       
-             const list = document.getElementById("usuarios");
-          const a = document.createElement('a');
-          const lista = document.createElement('li');
-          a.setAttribute('href', '#'); 
-          a.setAttribute('id', group.id); 
-          a.innerHTML = group.name;
-          lista.appendChild(a);
-          list.appendChild(lista); 
-    */
-        
         //}
       }
-   
+
     })
-     
-})
+
+  })
 };
 //users.addEventListener('click', (event)=>{
 // event.preventDefault();
